9834283f49421f855ee0feca75f04bd3d81d6418
LcEduardo <lucasproject777@gmail.com>
2026-02-22 08:41:20 -0300

    create a mehtod to create user

--- insert_user.php
+++ insert_user.php
@@ -4,32 +4,16 @@ require_once 'vendor/autoload.php';
 
 use App\Database\Connection;
 use App\Database\User;
+use App\Database\UserRepository;
 
 $pdo = Connection::getConnection();
+$c = new UserRepository($pdo);
 
-$userBatman = new User(
-    null, 'The Batman', 'thebatman@gmail.com', '09090', '2026-02-20', '1999542413', '1'
+$user = new User(
+    null, 'Alvin', 'alvinoesquilo@gmail.com', '21022026', '2026-02-21', '1999542411', '1'
 );
 
-try {
+$c->createUser($user);
 
-    $pdo = Connection::getConnection();
 
-    $sql = "INSERT INTO users (full_name, email, password, phone, created_at, status) 
-            VALUES (:full_name, :email, :password, :phone, :created_at, :status)";
 
-    $stmt = $pdo->prepare($sql);
-
-    $stmt->bindValue(':full_name', $userBatman->fullName(), PDO::PARAM_STR);
-    $stmt->bindValue(':email', $userBatman->email(), PDO::PARAM_STR);
-    $stmt->bindValue(':password', $userBatman->password(), PDO::PARAM_STR);
-    $stmt->bindValue(':phone', $userBatman->phone(), PDO::PARAM_STR);
-    $stmt->bindValue(':created_at', $userBatman->date(), PDO::PARAM_STR);
-    $stmt->bindValue(':status', $userBatman->status(), PDO::PARAM_STR);
-
-    $stmt->execute();
-
-    echo "New User Created";
-}catch(PDOException $e) {
-    $e->getMessage();
-}
--- src/UserRepository.php
+++ src/UserRepository.php
@@ -1,8 +1,12 @@
 <?php
 
+namespace App\Database;
+
 require_once 'vendor/autoload.php';
 
 use App\Database\User;
+use PDO;
+use PDOException;
 
 class UserRepository 
 {
@@ -14,5 +18,23 @@ class UserRepository
 
     public function createUser(User $user) {
 
+        try {
+            $sql = "INSERT INTO users (full_name, email, password, phone, created_at, status) 
+                    VALUES (:full_name, :email, :password, :phone, :created_at, :status)";
+
+            $stmt =  $this->connection->prepare($sql);
+            $stmt->bindValue(':full_name', $user->fullName(), PDO::PARAM_STR);
+            $stmt->bindValue(':email', $user->email(), PDO::PARAM_STR);
+            $stmt->bindValue(':password', $user->password(), PDO::PARAM_STR);
+            $stmt->bindValue(':phone', $user->phone(), PDO::PARAM_STR);
+            $stmt->bindValue(':created_at', $user->date(), PDO::PARAM_STR);
+            $stmt->bindValue(':status', $user->status(), PDO::PARAM_STR);
+
+            $stmt->execute();
+            echo "New User Created";
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+            
     }
 }
